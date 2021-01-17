28d9e47b58e5857dbd0df04ff049368e3ec37525
Tapan Pandya <tapanhpandya@gmail.com>
2021-01-17 14:37:55 -0500

    Display users from API

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
@@ -7,13 +7,57 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify from "aws-amplify";
+import Amplify, { API, Auth, graphqlOperation } from "aws-amplify";
 import config from "./src/aws-exports";
 Amplify.configure(config);
 
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
+
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get authenticated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      if (userInfo) {
+        // get the user from the Backend with the userid from Auth
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in database!!!");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Hey, I am using WhatsApp",
+        };
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+        // if there is no user in DB with the id, then create one
+      }
+    };
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -3,10 +3,25 @@ import { FlatList, StyleSheet } from "react-native";
 
 import { View } from "../components/Themed";
 import ContactListItem from "../components/ContactListItem";
-import users from "../data/Users";
-import NewMessageButton from "../components/NewMessageButton";
+import { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsers } from "../src/graphql/queries";
 
 export default function ChatsScreen() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const userData = await API.graphql(graphqlOperation(listUsers));
+        setUsers(userData.data.listUsers.items);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
